84af2d77542ce4b92063ad74fde9413b157169f2
acaiibawl <hagiya0719@gmail.com>
2022-02-22 13:58:45 +0900

    propsとemitによる親・子コンポーネント間の値の受け渡し

---
+++ components/child.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="">
+    <h1>{{ props.msg }}</h1>
+    <button @click="upperCaseMessage">To upper case</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext } from '@vue/composition-api';
+
+type Props = {
+  msg: string;
+}
+
+export default defineComponent({
+  name: 'ChildVue',
+  props: {
+    msg: {
+      type: String,
+      default: 'default Value.',
+      required: true,
+    },
+  },
+  setup: (props: Props, context: SetupContext) => {
+    const upperCaseMessage = () => {
+      context.emit('change-message', props.msg.toUpperCase());
+    }
+
+    return {
+      props,
+      upperCaseMessage
+    };
+  },
+});
+</script>
---
+++ pages/parent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ state.message }}</h1>
+    <child
+      :msg="state.message"
+      @change-message='changeMessage'
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "@vue/composition-api";
+import child from "~/components/child.vue";
+
+export default defineComponent({
+  name: 'ParentVue',
+  components: { child },
+  setup: () => {
+    const state = reactive({
+      message: 'Hello',
+    });
+
+    const changeMessage = (message: string) => {
+      state.message = message;
+    };
+
+    return {
+      state,
+      changeMessage,
+    }
+  }
+})
+</script>
